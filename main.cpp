75ed2e33e79eb3457fa2c0eebf33a45a659279b4
aryadamara <aryadamara@DESKTOP-PT9UTUQ>
2024-06-13 15:12:58 +0700

    membuat perintah

--- main.cpp
+++ main.cpp
@@ -4,3 +4,19 @@ using namespace std;
 
 
 
+class Node
+{
+    public:
+    string info;
+    Node* leftchild;
+    Node* rightchild;
+
+    // constructor fore the node class
+    Node(string i, Node* l, Node*r)
+    {
+        info = i;
+        leftchild =l;
+        rightchild = r;
+    }
+
+};
